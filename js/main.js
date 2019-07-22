bb6526fc78659b5eed00d2c10b7a0f5dde185e7f
davidjrada <davidjrada@gmail.com>
2019-07-22 19:48:01 -0400

    added ajax post request

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-
 const trackClick = () => {
-    let payload = btoa({
+    const payload = {
         "userId": "user1",
         "event": "Product Viewed",
         "properties": {
             "token": "f8da5fe54ec7b4e103fd94b369cc06cd",
-            "distinct_id": "user1",
+            "distinct_id": "user2",
             "product_id": "pr_507f1f77bcf86cd799439011",
             "sku": "G-32",
             "category": "Games",
@@ -15,17 +14,25 @@ const trackClick = () => {
             "price": 18.99,
             "quantity": 1
         }
-    })
-    console.log(payload)
-    fetch('https://api.mixpanel.com/track/?data=' + payload, {
-        method: 'POST',
-        event: "Product Viewed",
-        token: "f8da5fe54ec7b4e103fd94b369cc06cd",
-        headers: {
-            'Content-Type': "application/json"
-        }
-    }).catch(error => console.error(error))
+    }
+
+
+    $.post("https://api.mixpanel.com/track/",
+        {
+            data: btoa(JSON.stringify(payload)),
+        }, function (data, status) {
+            console.log(status);
+        })
 }
+
+
+
+
+
+
+
+//////////Previous Code using mixpanel variable////////
+
 
 // mixpanel.identify('user1');
 // mixpanel.track({
