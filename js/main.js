04ab8df6f5bcc537643425c4824f11b74a1d4313
davidjrada <davidjrada@gmail.com>
2019-07-22 18:02:04 -0400

    added festch request

--- js/main.js
+++ js/main.js
@@ -1,17 +1,45 @@
+
 const trackClick = () => {
-    mixpanel.track({
-            "userId": "user1",
-            "event": "Product Viewed",
-            "properties": {
-                "product_id": "pr_507f1f77bcf86cd799439011",
-                "sku": "G-32",
-                "category": "Games",
-                "name": "Monopoly: 3rd Edition",
-                "brand": "Hasbro",
-                "variant": "200 pieces",
-                "price": 18.99,
-                "quantity": 1
-            }
+    let payload = btoa({
+        "userId": "user1",
+        "event": "Product Viewed",
+        "properties": {
+            "token": "f8da5fe54ec7b4e103fd94b369cc06cd",
+            "distinct_id": "user1",
+            "product_id": "pr_507f1f77bcf86cd799439011",
+            "sku": "G-32",
+            "category": "Games",
+            "name": "Monopoly: 3rd Edition",
+            "brand": "Hasbro",
+            "variant": "200 pieces",
+            "price": 18.99,
+            "quantity": 1
         }
-    );
+    })
+    console.log(payload)
+    fetch('https://api.mixpanel.com/track/?data=' + payload, {
+        method: 'POST',
+        event: "Product Viewed",
+        token: "f8da5fe54ec7b4e103fd94b369cc06cd",
+        headers: {
+            'Content-Type': "application/json"
+        }
+    }).catch(error => console.error(error))
 }
+
+// mixpanel.identify('user1');
+// mixpanel.track({
+//     "userId": "user1",
+//     "event": "Product Viewed",
+//     "properties": {
+//         "product_id": "pr_507f1f77bcf86cd799439011",
+//         "sku": "G-32",
+//         "category": "Games",
+//         "name": "Monopoly: 3rd Edition",
+//         "brand": "Hasbro",
+//         "variant": "200 pieces",
+//         "price": 18.99,
+//         "quantity": 1
+//     }
+// }
+// );
